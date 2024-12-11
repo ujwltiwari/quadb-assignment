497474e236b10276753d1f00a4dbd5ba63bc2b96
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2024-12-11 22:40:55 +0530

    dockefile for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 5173,
-    host: true,
-    watch: {
-      usePolling: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    server: {
+      port: 5173,
+      host: true,
+      watch: {
+        usePolling: true,
+      },
+      esbuild: {
+        target: 'esnext',
+        platform: 'linux',
+      },
     },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    define: {
+      VITE_APP_BACKEND_ADDRESS: JSON.stringify(env.VITE_APP_BACKEND_ADDRESS),
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 })
